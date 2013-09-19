83d76c95379784b40d1a1ad1bbc0c29962955331
Joe Koberg <joe.koberg@versionone.com>
2013-09-19 15:14:40 -0400

    Initial commit of Yaml/Json Csharp example

---
+++ csharp/VersionOne.Example.ItemsOpenInProject/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nito.AsyncEx.Synchronous;
+using OAuth2Client;
+using Microsoft.FSharp.Core;
+
+namespace VersionOne.Example.ItemsOpenInProject
+{
+	internal class Program
+	{
+		public const string ApiEndpoint = "https://www14.v1host.com/v1sdktesting/query.v1";
+
+		public const string ApiQuery = @"
+from: PrimaryWorkitem
+where:
+  Scope.Name: ""Sample: Release 1.0""
+  Scope.AssetState: Active
+select:
+  - AssetType
+  - Name
+  - Estimate
+";
+
+		private static async Task DoRequest()
+		{
+			Console.WriteLine(ApiQuery);
+			var storage = new FSharpOption<IStorageAsync>(new Storage.JsonFileStorage(@"..\..\client_secrets.json", @"..\..\stored_credentials.json"));
+			var httpclient = OAuth2Client.HttpClientFactory.WithOAuth2("query-api-1.0", storage, null, null);
+
+			var response = await httpclient.PostAsync(ApiEndpoint, new StringContent(ApiQuery));
+			if (response.StatusCode != HttpStatusCode.OK)
+			{
+				var errorBody = await response.Content.ReadAsStringAsync();
+				throw new Exception("Request failed:\n" + errorBody);
+			}
+
+			var jsonBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(jsonBody);
+			var resultSets = JArray.Parse(jsonBody);
+			var results =
+				from item in resultSets[0]
+				select new {Name = item["Name"], Estimate = item["Estimate"]}
+				;
+
+			foreach (var item in results)
+			{
+				Console.WriteLine("{0} {1}", item.Name, item.Estimate);
+			}
+		}
+
+		private static void Main(string[] args)
+		{
+			DoRequest().WaitAndUnwrapException();
+		}
+	}
+}
---
+++ csharp/VersionOne.Example.ItemsOpenInProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VersionOne.Example.ItemsOpenInProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VersionOne.Example.ItemsOpenInProject")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a995da9d-3915-4b36-9a27-93fb6f53cbb9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
