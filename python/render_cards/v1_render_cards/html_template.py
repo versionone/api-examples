ebdfe2fcd63fb22d909b40c3417a4b58b0f75d37
Joe Koberg <joe.koberg@versionone.com>
2013-09-26 08:49:16 -0400

    fix spacing

--- python/render_cards/v1_render_cards/html_template.py
+++ python/render_cards/v1_render_cards/html_template.py
@@ -1,95 +1,95 @@
 def head():
-	return """
+    return """
 <html>
-	<head>
-		<style type="text/css">
-			body {
-				width: 11in;
-				height: 8.5in;
-				margin: 0;
-				padding: 0;
-				font-family: sans-serif;
-			}
+    <head>
+        <style type="text/css">
+            body {
+                width: 11in;
+                height: 8.5in;
+                margin: 0;
+                padding: 0;
+                font-family: sans-serif;
+            }
 
-		    div.card{
-		    	width: 5.0in;
-		    	height: 3.75in;
-		    	padding: 0.25in;
-		    	float: left;
-		    	}
+            div.card{
+                width: 5.0in;
+                height: 3.75in;
+                padding: 0.25in;
+                float: left;
+                }
 
-		    .onleft {
-		    	clear: left;
-			    }
+            .onleft {
+                clear: left;
+                }
 
-		    div.descriptext {
-		    	width: 100%;
-		    	height: 67%;
-		    	}
+            div.descriptext {
+                width: 100%;
+                height: 67%;
+                }
 
-		    img.qrcode {
-		    	float: left;
-		    	}
+            img.qrcode {
+                float: left;
+                }
 
-		    table {
-		    	height: 33%;
-		    	width: 100%;
-		    	border-collapse: collapse;
-		        }
+            table {
+                height: 33%;
+                width: 100%;
+                border-collapse: collapse;
+                }
 
-		    td {
-		    	font-weight: bold;
-		    	text-align: center;
-		    	border: 1pt solid grey;
-		        }
+            td {
+                font-weight: bold;
+                text-align: center;
+                border: 1pt solid grey;
+                }
 
-		    tr.details td {
-		    	font-size: 12pt;
-		    }
+            tr.details td {
+                font-size: 12pt;
+            }
 
-		    tr.owners td {
-		    	font-size: 18pt;
-		    }
+            tr.owners td {
+                font-size: 18pt;
+            }
 
-		    tr.title td {
-		    	font-size: 36pt;
-		    }
+            tr.title td {
+                font-size: 36pt;
+            }
 
-		</style>
-	</head>
-	<body>
+        </style>
+    </head>
+    <body>
 """
 
 import urllib
 
 def card(title="", description="", todo="", done="", priority="", owners="", qrdata="", onleft=true, qrsize=100):
-	onleft = "onleft" if onleft else ""
-	owners = ", ".join(owners)
-	qrdata = urllib.urlencode(qrdata)
+    onleft = "onleft" if onleft else ""
+    owners = ", ".join(owners)
+    qrdata = urllib.urlencode(qrdata)
     return """
-		<div class="card %(onleft)s">
-			<div class="descriptext"><img class="qrcode" src="https://chart.googleapis.com/chart?cht=qr&chl=%(qrdata)s&chs=%(qrsize)dx%(qrsize)d&choe=UTF-8&chld=L|2">
-				%(description)s
-			</div>
-			<table>
-				<tr class="details">
-					<td >ToDo: %(todo)s</td>
-					<td >Done: %(done)s</td>
-					<td >Pri: %(priority)s</td>
-				</tr>
-				<tr class="owners">
-					<td colspan="3" >Owners: %(owners)s</td>
-				</tr>
-				<tr class="title">
-					<td colspan="3" >%(title)s</td>
-				</tr>
-			</table>
-		</div>
-		"""%locals()
+        <div class="card %(onleft)s">
+            <div class="descriptext"><img class="qrcode" src="https://chart.googleapis.com/chart?cht=qr&chl=%(qrdata)s&chs=%(qrsize)dx%(qrsize)d&choe=UTF-8&chld=L|2">
+                %(description)s
+            </div>
+            <table>
+                <tr class="details">
+                    <td >ToDo: %(todo)s</td>
+                    <td >Done: %(done)s</td>
+                    <td >Pri: %(priority)s</td>
+                </tr>
+                <tr class="owners">
+                    <td colspan="3" >Owners: %(owners)s</td>
+                </tr>
+                <tr class="title">
+                    <td colspan="3" >%(title)s</td>
+                </tr>
+            </table>
+        </div>
+        """%locals()
 
 
 def tail():
-    return """		
-	</body>
+    return """        
+    </body>
 </html>
 """
--- python/render_cards/v1_render_cards/render_cards_html.py
+++ python/render_cards/v1_render_cards/render_cards_html.py
@@ -5,35 +5,35 @@ import html_template
 import v1pysdk
 
 def getCards():
-	with v1pysdk.V1Meta(...) as v1:
-		for row in v1.Task.where(...).select("Name", "Description", "Todo", "Done", "Owners.Name"):
-			yield {
-				title: row.Title,
-				description: row.Description,
-				todo: row.Todo,
-				done: row.Done,
-				priority: row.Priority,
-				owners: row.data["Owners.Name"],
-				qrdata: ""
-			}
+    with v1pysdk.V1Meta(...) as v1:
+        for row in v1.Task.where(...).select("Name", "Description", "Todo", "Done", "Owners.Name"):
+            yield {
+                title: row.Title,
+                description: row.Description,
+                todo: row.Todo,
+                done: row.Done,
+                priority: row.Priority,
+                owners: row.data["Owners.Name"],
+                qrdata: ""
+            }
 
 
 def items():
-	yield html_template.head()
-	for n, row in enumerate(getCards()):
-		onleft = (n % 2 == 0)
-		row["onleft"] = onleft
-		yield html_template.card(**row)
-	yield html_template.tail()
+    yield html_template.head()
+    for n, row in enumerate(getCards()):
+        onleft = (n % 2 == 0)
+        row["onleft"] = onleft
+        yield html_template.card(**row)
+    yield html_template.tail()
 
 def render():
-	return "\n".join(items())
+    return "\n".join(items())
 
 def main():
-	html_out = render()
-	print html_out
+    html_out = render()
+    print html_out
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
