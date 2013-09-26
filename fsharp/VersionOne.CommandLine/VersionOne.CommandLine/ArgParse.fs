fe5f9de78c82174e0e58ca3ebd59f9f4038f9336
Joe Koberg <joe.koberg@versionone.com>
2013-09-26 08:06:35 -0400

    Command line client for query.v1

---
+++ fsharp/VersionOne.CommandLine/VersionOne.CommandLine/ArgParse.fs
@@ -0,0 +1,41 @@
+﻿module ArgParse
+
+///
+/// Command line option parsing library
+///
+/// Create an OptParse<'Options> object when you 
+///
+type OptParse<'Options> = {
+    Default   : 'Options
+    Switches  : Map<string, 'Options -> 'Options>
+    Values    : Map<string, string -> 'Options -> 'Options>
+    Other     : (string -> 'Options -> 'Options)
+    Error     : (string -> 'Options -> 'Options)
+  } with
+
+  member x.isSwitch = x.Switches.ContainsKey
+
+  member x.isValue = x.Values.ContainsKey
+
+  member x.splitEq (s:string) =
+    match s.Split([|'='|], 2) with
+    | [| s |] -> false, s, ""
+    | [| k; v |] -> (x.isValue k), k, v
+
+  member x.isEquals s = let p,_,_ = x.splitEq s in p
+
+  member x.handleEq e = 
+    let p,f,v = x.splitEq e
+    if p then x.Values.[f] v else x.Error f
+
+  member x.ofArray (argv:string[]) =
+    let rec parse args opts =
+      match args with
+      |      f :: more when x.isSwitch f      -> opts |> x.Switches.[f]  |> parse more
+      |      e :: more when x.isEquals e      -> opts |> x.handleEq e    |> parse more
+      |      f :: []   when x.isValue f       -> opts |> x.Error f       |> parse [] 
+      | f :: v :: more when x.isValue  f      -> opts |> x.Values.[f] v  |> parse more
+      |      d :: more when d.StartsWith("-") -> opts |> x.Error d       |> parse more
+      |      o :: more                        -> opts |> x.Other o       |> parse more
+      |             []                        -> opts
+    x.Default |> parse (List.ofArray argv)
---
+++ fsharp/VersionOne.CommandLine/VersionOne.CommandLine/Program.fs
@@ -0,0 +1,72 @@
+﻿module VersionOne.CommandLine
+
+open OAuth2Client
+
+open ArgParse
+
+let await = Async.AwaitTask
+let start = Async.StartAsTask
+
+type Options = {
+  url     : string
+  creds   : string
+  secrets : string
+  scope   : string
+  endpoint : string
+  quiet : bool
+  outputs : string list
+  errors  : string list
+  }
+
+let optionDef = {
+  Default = { url     = "https://www14.v1host.com/v1sdktesting"
+              creds   = "stored_credentials.json"
+              secrets = "client_secrets.json"
+              scope   = "query-api-1.0"
+              endpoint = "/query.v1"
+              quiet = false
+              outputs = []
+              errors  = [] }
+  Switches = Map [ "-h",       fun   o -> failwith "Would display help here"
+                   "-q",       fun   o -> {o with quiet = true} ]
+  Values = Map [ "--url",      fun v o -> {o with url = v}
+                 "--scope",    fun v o -> {o with scope = v}
+                 "--endpoint", fun v o -> {o with endpoint = v}
+                 "--creds",    fun v o -> {o with creds = v}
+                 "--secrets",  fun v o -> {o with secrets = v} ]
+  Other =                      fun v o -> {o with outputs = v::o.outputs}
+  Error =                      fun v o -> printfn "WARNING: Ignored argument: %s" v
+                                          {o with errors = v::o.errors}
+  }
+
+open System
+open System.Net
+open System.Net.Http
+
+let mainAsync argv = start <| async {
+  let opts = optionDef.ofArray argv
+  let storage = Storage.JsonFileStorage(opts.secrets, opts.creds)
+  let client = HttpClient.WithOAuth2(opts.scope, storage)
+  let endpoint = opts.url.TrimEnd([|'/'|]) + "/" + opts.endpoint.TrimStart([|'/'|])
+  if not opts.quiet then
+    eprintfn "Will make query to %s" endpoint
+    eprintfn "Enter the query.  Query will be sent at end of input."
+  let! input = await <| Console.In.ReadToEndAsync()
+  use content = new StringContent(input)
+  let! response = await <| client.PostAsync(endpoint, content)
+  if response.StatusCode <> HttpStatusCode.OK then
+    if not opts.quiet then
+      eprintfn "An error occurred.\n%A" response
+    return 1
+  else
+    let! body = await <| response.Content.ReadAsStringAsync()
+    let! success = Async.AwaitIAsyncResult <| Console.Out.WriteAsync(body)
+    return 0
+  }
+
+
+open Nito.AsyncEx.Synchronous
+
+[<EntryPoint>]
+let main argv = (mainAsync argv).WaitAndUnwrapException()
+
