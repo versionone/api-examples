cace3914de0ca91eb43ad3df20d998b283989700
Joe Koberg <joe.koberg@versionone.com>
2014-01-13 16:46:33 -0500

    Refactor the non-generic behavior out of GetResults; re-indent with spaces

--- csharp/CapOneQueryExample/CapOneQueryExample/Program.cs
+++ csharp/CapOneQueryExample/CapOneQueryExample/Program.cs
@@ -8,53 +8,48 @@ using Nito.AsyncEx.Synchronous;
 
 namespace CapOneQueryExample
 {
-	internal class Program
+  internal class Program
   {
-		/// <summary>
-		/// Command line Main().
-		/// Just calls MainAsync() with Nito.AsyncEx Task.WaitAndUnwrapException()
-		/// so we properly run the code in the background and get proper error notifications.
-		/// </summary>
-		/// <param name="args">Arguments from the command line</param>
+    /// <summary>
+    /// Command line Main().
+    /// Just calls MainAsync() with Nito.AsyncEx Task.WaitAndUnwrapException()
+    /// so we properly run the code in the background and get proper error notifications.
+    /// </summary>
+    /// <param name="args">Arguments from the command line</param>
     private static void Main(string[] args)
     {
       var mainTask = MainAsync(args);
       mainTask.WaitAndUnwrapException();
     }
 
-		/// <summary>
-		/// Async program entry point.
-		/// We use async here because we need to use the Microsoft System.Net.Http library,
-		/// which only exports async methods.
-		/// </summary>
-		/// <param name="args">Unused, but left in to match the signature of Main()</param>
-		/// <returns>A Task that is already running the report.</returns>
+    /// <summary>
+    /// Async program entry point.
+    /// We use async here because we need to use the Microsoft System.Net.Http library,
+    /// which only exports async methods.
+    /// </summary>
+    /// <param name="args">Unused, but left in to match the signature of Main()</param>
+    /// <returns>A Task that is already running the report.</returns>
     private static async Task MainAsync(string[] args)
     {
-			var results = await GetResults<ThreeIterationReport.StoryDto>();
-	    var stories = results[0];
-      foreach (var storyDto in stories) Console.WriteLine(storyDto);
+      var url = "https://www14.v1host.com/v1sdktesting/query.legacy.v1";
+      var queryBody = ThreeIterationReport.Query;
+      var results = await GetResults<ThreeIterationReport.StoryDto>(url, queryBody);
+      var stories = results[0];
+      foreach (var storyDto in stories)
+          Console.WriteLine(storyDto);
     }
 
-    private const string QueryTest = @"
- from: Member
- where:
-  Name: admin
- select: 
-  - Name
-  - Nickname
-";
-
-    static async Task<List<List<T>>> GetResults<T>()
+    static async Task<List<List<T>>> GetResults<T>(string url, string queryBody, string username="admin", string password="admin")
     {
-      var queryBody = new StringContent(ThreeIterationReport.Query);
-      using (var handler = new HttpClientHandler { Credentials = new NetworkCredential("admin", "admin") })
-      using (var client = new HttpClient(handler))
-      {
-        var httpResp = await client.PostAsync("https://www14.v1host.com/v1sdktesting/query.legacy.v1", queryBody);
-        var httpBody = await httpResp.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<List<List<T>>>(httpBody);
-      }
+        using (var handler = new HttpClientHandler {Credentials = new NetworkCredential("username", "password")})
+        {
+            using (var client = new HttpClient(handler))
+            {
+                var httpResp = await client.PostAsync(url, new StringContent(queryBody));
+                var httpBody = await httpResp.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<List<T>>>(httpBody);
+            }
+        }
     }
   }
 }
--- csharp/CapOneQueryExample/CapOneQueryExample/ThreeIterationReport.cs
+++ csharp/CapOneQueryExample/CapOneQueryExample/ThreeIterationReport.cs
@@ -5,9 +5,9 @@ using Newtonsoft.Json;
 
 namespace CapOneQueryExample
 {
-	public static class ThreeIterationReport
-	{
-		public static string Query = @"
+  public static class ThreeIterationReport
+  {
+    public static string Query = @"
             with:
               $teams: whatever
               $sprintName: whatever
@@ -47,88 +47,88 @@ namespace CapOneQueryExample
                       - Description
             ";
 
-		public class StoryDto
-		{
-			public string Number { get; set; }
+    public class StoryDto
+    {
+      public string Number { get; set; }
 
-			[JsonProperty("Status.Name")]
-			public string Status_Name { get; set; }
+      [JsonProperty("Status.Name")]
+      public string Status_Name { get; set; }
 
-			[JsonProperty("Team.Name")]
-			public string Team_Name { get; set; }
+      [JsonProperty("Team.Name")]
+      public string Team_Name { get; set; }
 
-			public decimal Estimate { get; set; }
+      public decimal Estimate { get; set; }
 
-			[JsonProperty("Priority.Name")]
-			public string Priority_Name { get; set; }
+      [JsonProperty("Priority.Name")]
+      public string Priority_Name { get; set; }
 
-			[JsonProperty("Priority.Order")]
-			public decimal Priority_Order { get; set; }
+      [JsonProperty("Priority.Order")]
+      public decimal Priority_Order { get; set; }
 
-			[JsonProperty("Children:Task")]
-			public List<TaskDto> Tasks { get; set; }
+      [JsonProperty("Children:Task")]
+      public List<TaskDto> Tasks { get; set; }
 
-			[JsonProperty("Super:Epic")]
-			public List<EpicDto> ParentEpic { get; set; }
+      [JsonProperty("Super:Epic")]
+      public List<EpicDto> ParentEpic { get; set; }
 
-			public class TaskDto
-			{
-				public string Type { get; set; }
+      public class TaskDto
+      {
+        public string Type { get; set; }
 
-				public string Name { get; set; }
+        public string Name { get; set; }
 
-				[JsonProperty("Status.Name")]
-				public string Status_Name { get; set; }
+        [JsonProperty("Status.Name")]
+        public string Status_Name { get; set; }
 
-				[JsonProperty("Owners.Name")]
-				public List<string> Owners_Name { get; set; }
+        [JsonProperty("Owners.Name")]
+        public List<string> Owners_Name { get; set; }
 
-				public decimal DetailEstimate { get; set; }
+        public decimal DetailEstimate { get; set; }
 
-				[JsonProperty("Actuals.Value.@Sum")]
-				public decimal Actuals_Value_Sum { get; set; }
+        [JsonProperty("Actuals.Value.@Sum")]
+        public decimal Actuals_Value_Sum { get; set; }
 
-				public decimal ToDo { get; set; }
-			}
+        public decimal ToDo { get; set; }
+      }
 
-			public class EpicDto
-			{
-				public string Number { get; set; }
+      public class EpicDto
+      {
+        public string Number { get; set; }
 
-				public string Name { get; set; }
+        public string Name { get; set; }
 
-				[JsonProperty("Subs:PrimaryWorkitem.Estimate.@Sum")]
-				public string Subs_PrimaryWorkitem_Estimate_Sum { get; set; }
+        [JsonProperty("Subs:PrimaryWorkitem.Estimate.@Sum")]
+        public string Subs_PrimaryWorkitem_Estimate_Sum { get; set; }
 
-				public decimal Swag { get; set; }
+        public decimal Swag { get; set; }
 
-				[JsonProperty("Scope.Name")]
-				public string Scope_Name { get; set; }
+        [JsonProperty("Scope.Name")]
+        public string Scope_Name { get; set; }
 
-				public string AssetType { get; set; }
+        public string AssetType { get; set; }
 
-				public string Description { get; set; }
-			}
+        public string Description { get; set; }
+      }
 
-			public override string ToString()
-			{
-				var sb = new StringBuilder();
-				sb.AppendLine(this.Number);
-				sb.AppendLine(this.Team_Name);
-				sb.AppendLine(this.Estimate.ToString());
+      public override string ToString()
+      {
+        var sb = new StringBuilder();
+        sb.AppendLine(this.Number);
+        sb.AppendLine(this.Team_Name);
+        sb.AppendLine(this.Estimate.ToString());
 
-				var epic = this.ParentEpic.First();
-				sb.AppendLine(epic.Number);
-				sb.AppendLine(epic.Swag.ToString());
+        var epic = this.ParentEpic.First();
+        sb.AppendLine(epic.Number);
+        sb.AppendLine(epic.Swag.ToString());
 
-				foreach (var taskDto in this.Tasks)
-				{
-					sb.AppendLine(taskDto.Name);
-					sb.AppendLine(taskDto.Actuals_Value_Sum.ToString());
-				}
+        foreach (var taskDto in this.Tasks)
+        {
+          sb.AppendLine(taskDto.Name);
+          sb.AppendLine(taskDto.Actuals_Value_Sum.ToString());
+        }
 
-				return sb.ToString();
-			}
-		}
-	}
+        return sb.ToString();
+      }
+    }
+  }
 }
